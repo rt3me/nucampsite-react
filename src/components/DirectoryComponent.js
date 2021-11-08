a22f354be1d5506d9873e4dfe72ec690d7e208c8
Robert Turner <robturneris@gmail.com>
2021-11-07 21:56:59 -0500

    Added event listener to Card component in DirectoryComponent.js

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -5,15 +5,19 @@ class Directory extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      selectedCampsite: null
     };
+  }
+
+  onCampsiteSelect (campsite) {
+    this.setState({selectedCampsite: campsite});
   }
 
   render() {
     const directory = this.props.campsites.map((campsite) => {
       return (
         <div key={campsite.id} className="col-md-5 m-1">
-          <Card>
+          <Card onClick={() => this.onCampsiteSelect(campsite)}>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
               <CardTitle>{campsite.name}</CardTitle>
