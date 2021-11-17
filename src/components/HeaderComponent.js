e33ff39500fa5bdd082328d2c37a722603558ad5
Robert Turner <robturneris@gmail.com>
2021-11-16 21:11:58 -0500

    Added header component

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Navbar, NavbarBrand, Jumbotron } from "reactstrap";
+
+class Header extends Component {
+  render() {
+    return(
+
+    );
+  }
+}
