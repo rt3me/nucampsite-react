85c1d4cbde05c683fd76a3ca4f8cc6cc98932600
Robert Turner <robturneris@gmail.com>
2021-11-17 22:52:50 -0500

    Changed jsx file names to jsx extensions

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -4,7 +4,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { CAMPSITES } from '../shared/campsites.js';
+import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
   constructor(props) {
