e7f14d4cc86e20b26d2e237722ab2d2aa121bf40
Robert Turner <robturneris@gmail.com>
2021-11-22 17:48:59 -0600

    Updated CampsiteInfo component to render comments from props

--- src/components/CampsiteInfoComponent.jsx
+++ src/components/CampsiteInfoComponent.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
-function RenderCampsite({campsite}) {
+function RenderCampsite({ campsite }) {
   return (
     <div className="col-md-5 m-1">
       <Card>
@@ -15,16 +15,18 @@ function RenderCampsite({campsite}) {
   );
 }
 
-function RenderComments({comments}) {
+function RenderComments({ comments }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
         <h4>Comments</h4>
-        {comments.map(comment => {
+        {comments.map((comment) => {
           return (
             <div key={comment.id}>
-              <p>{comment.text}<br />
-              -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+              <p>
+                {comment.text}
+                <br />
+                -- {comment.author}, {new Intl.DateTimeFormat("en-US", { year: "numeric", month: "short", day: "2-digit" }).format(new Date(Date.parse(comment.date)))}
               </p>
             </div>
           );
@@ -35,13 +37,13 @@ function RenderComments({comments}) {
   return <div />;
 }
 
-function  CampsiteInfo(props) {
+function CampsiteInfo(props) {
   if (props.campsite) {
     return (
       <div className="container">
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.campsite.comments} />
+          <RenderComments comments={props.comments} />
         </div>
       </div>
     );
