b84e31f712b0e30f9b0f0e15c6366760c5461f91
Robert Turner <robturneris@gmail.com>
2021-11-27 13:56:35 -0600

    Removed state and related imports from Main component

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -7,22 +7,8 @@ import Contact from "./ContactComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import About from "./AboutComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { CAMPSITES } from "../shared/campsites";
-import { COMMENTS } from "../shared/comments";
-import { PROMOTIONS } from "../shared/promotions";
-import { PARTNERS } from "../shared/partners";
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      partners: PARTNERS,
-    };
-  }
-
   render() {
     const HomePage = () => {
       return <Home campsite={this.state.campsites.filter((campsite) => campsite.featured)[0]} promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]} partner={this.state.partners.filter((partner) => partner.featured)[0]} />;
