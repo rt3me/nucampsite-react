3f07016cdcecffa10311f007d0dba2697d95edad
Robert Turner <robturneris@gmail.com>
2021-12-04 23:08:35 -0500

    Added Loading component, error message, and props to Home component

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Loading } from "./LoadingComponent";
 
-function RenderCard({ item }) {
+function RenderCard({ item, isLoading, errMess }) {
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (errMess) {
+    return <h4>{errMess}</h4>;
+  }
   return (
     <Card>
       <CardImg src={item.image} alt={item.name} />
@@ -17,13 +24,13 @@ function Home(props) {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md m-1">
-          <RenderCard item={props.campsite} />
+        <div className="m-1 col-md">
+          <RenderCard item={props.campsite} isLoading={props.campsitesLoading} errMess={props.campsitesErrMess} />
         </div>
-        <div className="col-md m-1">
+        <div className="m-1 col-md">
           <RenderCard item={props.promotion} />
         </div>
-        <div className="col-md m-1">
+        <div className="m-1 col-md">
           <RenderCard item={props.partner} />
         </div>
       </div>
