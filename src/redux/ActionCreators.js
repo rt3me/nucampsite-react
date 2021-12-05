617c70a10183b27955ca84e99bd3757113375858
Robert Turner <robturneris@gmail.com>
2021-12-04 22:01:35 -0500

    Added loading, failed, and add campsite action creators and fetch campsites

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from "./ActionTypes";
+import { CAMPSITES } from "../shared/campsites";
 
 export const addComment = (campsiteId, rating, author, text) => ({
   type: ActionTypes.ADD_COMMENT,
@@ -8,4 +9,26 @@ export const addComment = (campsiteId, rating, author, text) => ({
     author, // pass like this
     text, // Shorthand property names
   },
+});
+
+export const fetchCampsites = () => (dispatch) => {
+  dispatch(campsitesLoading());
+
+  setTimeout(() => {
+    dispatch(addCampsite(CAMPSITES));
+  }, 2000);
+};
+
+export const campsitesLoading = () => ({
+  type: ActionTypes.CAMPSITES_LOADING,
+});
+
+export const campsitesFailed = (errMess) => ({
+  type: ActionTypes.CAMPSITES_FAILED,
+  payload: errMess,
+});
+
+export const addCampsite = (campsites) => ({
+  type: ActionTypes.ADD_CAMPSITE,
+  payload: campsites,
 });
