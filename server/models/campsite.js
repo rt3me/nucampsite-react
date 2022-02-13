4e7c13ad2a36fada89916b21da92f99dfa5c8d3c
Robert Turner <robturneris@gmail.com>
2022-02-12 22:16:00 -0500

    Prepping models for client server integration

--- server/models/campsite.js
+++ server/models/campsite.js
@@ -1,29 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 require("mongoose-currency").loadType(mongoose);
 const Currency = mongoose.Types.Currency;
-
-const commentSchema = new Schema(
-  {
-    rating: {
-      type: Number,
-      min: 1,
-      max: 5,
-      required: true,
-    },
-    text: {
-      type: String,
-      required: true,
-    },
-    author: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
 
 const campsiteSchema = new Schema(
   {
@@ -53,7 +32,6 @@ const campsiteSchema = new Schema(
       type: Boolean,
       default: false,
     },
-    comments: [commentSchema],
   },
   {
     timestamps: true,
