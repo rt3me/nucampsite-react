5661bd963fe3207e45332be91e1177a74b8490a4
Robert Turner <robturneris@gmail.com>
2022-02-12 22:22:09 -0500

    Prepping routers for client server integration

--- server/routes/cors.js
+++ server/routes/cors.js
@@ -1,15 +1,15 @@
-const cors = require("cors");
+const cors = require('cors');
 
-const whitelist = ["http://localhost:3000", "https://localhost:3443"];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443', 'http://localhost:3001'];
 const corsOptionsDelegate = (req, callback) => {
-  let corsOptions;
-  console.log(req.header("Origin"));
-  if (whitelist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true };
-  } else {
-    corsOptions = { origin: false };
-  }
-  callback(null, corsOptions);
+    let corsOptions;
+    console.log(req.header('Origin'));
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true };
+    } else {
+        corsOptions = { origin: false };
+    }
+    callback(null, corsOptions);
 };
 
 exports.cors = cors();
